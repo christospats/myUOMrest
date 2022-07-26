9f667db4f6a4070cb2d8e3596ce145b1a2f45eb0
christos fanourios patsouras <xristos.patsoyras@gmail.com>
2022-07-26 19:32:53 +0300

    3rdver

--- menu-app/src/Menu.js
+++ menu-app/src/Menu.js
@@ -5,15 +5,7 @@ import {
     AccordionButton,
     AccordionPanel,
     AccordionIcon,
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer
+    Accordion
   } from '@chakra-ui/react';
 
 class Menu extends Component {
@@ -32,38 +24,50 @@ class Menu extends Component {
                   </AccordionButton>
                 </h2>
                 <AccordionPanel pb={4}>
-                  <TableContainer>
-                    <Table variant='simple' align='center'>
-                      <Thead></Thead>
-                      <Tbody>
-                        <Td>
-                          <Tr>Γεύμα κυρίως: </Tr>
-                          <Tr>Γεύμα ειδικό: </Tr>
-                          <Tr>Γεύμα γαρνιτούρα: </Tr>
-                          <Tr>Γεύμα σαλάτα: </Tr>
-                          <Tr>Γεύμα επιδόρπιο: </Tr>
-                          <Tr>Δείπνο κυρίως: </Tr>
-                          <Tr>Δείπνο ειδικό: </Tr>
-                          <Tr>Δείπνο γαρνιτούρα: </Tr>
-                          <Tr>Δείπνο σαλάτα: </Tr>
-                          <Tr>Δείπνο επιδόρπιο: </Tr>
-                        </Td>
-                        <Td>
-                          <Tr>{this.props.gevmakirios}</Tr>
-                          <Tr>{this.props.gevmaEidiko} </Tr>
-                          <Tr>{this.props.gevmaGarnitoura} </Tr>
-                          <Tr>{this.props.gevmaSalata} </Tr>
-                          <Tr>{this.props.gevmaEpidorpio} </Tr>
-                          <Tr>{this.props.deipnoKirios} </Tr>
-                          <Tr>{this.props.deipnoEidiko} </Tr>
-                          <Tr>{this.props.deipnoGarnitoura} </Tr>
-                          <Tr>{this.props.deipnoSalata} </Tr>
-                          <Tr>{this.props.deipnoEpidorpio}</Tr>
-                        </Td>
-                      </Tbody>
-                      <Tfoot></Tfoot>
-                    </Table>
-                  </TableContainer>
+
+                  <Accordion defaultIndex={[0]} allowToggle>
+                    {/*Accordion gia geuma*/}
+                    <AccordionItem>
+                      <h2>
+                        <AccordionButton _expanded={{ bg: 'blue', color: 'yellow' }}>
+                          <Box flex='1' textAlign='center' fontSize='large'>
+                          Γεύμα
+                          </Box>
+                        <AccordionIcon />
+                        </AccordionButton>
+                      </h2>
+                      <AccordionPanel pb={4}>
+                        <div>
+                          {this.props.gevmakirios}<br></br>
+                          {this.props.gevmaEidiko}<br></br>
+                          {this.props.gevmaGarnitoura}<br></br>
+                          {this.props.gevmaSalata}<br></br>
+                          {this.props.gevmaEpidorpio} 
+                        </div>
+                      </AccordionPanel>
+                   </AccordionItem>
+                    {/*Accordion gia deipno*/}
+                   <AccordionItem>
+                      <h2>
+                        <AccordionButton _expanded={{ bg: 'blue', color: 'yellow' }}>
+                          <Box flex='1' textAlign='center' fontSize='large'>
+                          Δείπνο
+                          </Box>
+                        <AccordionIcon />
+                        </AccordionButton>
+                      </h2>
+                      <AccordionPanel pb={4}>
+                        <div>
+                          {this.props.deipnoKirios}<br></br>
+                          {this.props.deipnoEidiko}<br></br>
+                          {this.props.deipnoGarnitoura}<br></br>
+                          {this.props.deipnoSalata}<br></br>
+                          {this.props.deipnoEpidorpio} 
+                        </div>
+                      </AccordionPanel>
+                   </AccordionItem>
+                  </Accordion>
+
                 </AccordionPanel>
               </AccordionItem>
             </>
